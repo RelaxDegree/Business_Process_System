f843e43decb2094a4fff7d97d69a646c0021365b
FY-Rzz <2244151126@qq.com>
2023-02-27 09:50:56 +0800

    1

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,5 +1,5 @@
 <template>
-    <!-- <el-container>
+  <!-- <el-container>
         <el-aside width="auto">
             <com-aside />
         </el-aside>
@@ -13,14 +13,13 @@
             </el-main>
         </el-container>
     </el-container> -->
-    <el-container>
-        <el-header><big-header></big-header></el-header>
-        <el-main>
-            <!-- 路由出口 -->
-            <router-view></router-view>
-        </el-main>
-        <el-footer>Footer</el-footer>
-    </el-container>
+  <el-container>
+    <el-header><big-header></big-header></el-header>
+    <el-main>
+      <router-view></router-view>
+    </el-main>
+    <el-footer> </el-footer>
+  </el-container>
 </template>
 <script>
 import BigHeader from "../components/BigHeader.vue";
@@ -28,19 +27,19 @@ import PersonAvatar from "../components/PersonAvatar.vue";
 import ComAside from "../components/ComAside.vue";
 import ComHeader from "../components/ComHeader.vue";
 export default {
-    data() {
-        return {}
-    },
-    components: {
-        BigHeader,
-        PersonAvatar,
-        ComAside,
-        ComHeader
-    }
-}
+  data() {
+    return {};
+  },
+  components: {
+    BigHeader,
+    PersonAvatar,
+    ComAside,
+    ComHeader,
+  },
+};
 </script>
 <style scoped>
 .el-header {
-    padding: 0;
+  padding: 0;
 }
 </style>
